63a18b8cfe82eb0d1d7cf16de965c4489bae88b1
elen-oz <ozerova.lenka@gmail.com>
2024-02-11 12:54:14 +0100

    finish first project

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
-import CourseGoal from './CourseGoal';
 import { type CourseGoal as CGoal } from '../App';
+import { type ReactNode } from 'react';
+import CourseGoal from './CourseGoal';
+import InfoBox from './InfoBox';
 
 interface Props {
   goals: CGoal[];
@@ -7,19 +9,40 @@ interface Props {
 }
 
 const CourseGoalList = ({ goals, onDeleteGoal }: Props) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning' severity='medium'>
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal
-            id={goal.id}
-            title={goal.title}
-            description={goal.description}
-            onDelete={onDeleteGoal}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal
+              id={goal.id}
+              title={goal.title}
+              description={goal.description}
+              onDelete={onDeleteGoal}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 export default CourseGoalList;
